1687449681e7c3bcef5ecf7d0e6b9ed1316a9b67
Jack <jackedmunds2013@gmail.com>
2026-09-05 13:57:30 +0100

    Register HatEntity and update mod ID

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,28 +1,31 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
-import net.minecraft.resources.Identifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    public static final String MOD_ID = "hatmod";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final EntityType<HatEntity> HAT_ENTITY = Registry.register(
+            BuiltInRegistries.ENTITY_TYPE,
+            new ResourceLocation(MOD_ID, "hat"),
+            FabricEntityTypeBuilder.create(MobCategory.CREATURE, HatEntity::new)
+                    .dimensions(EntityDimensions.scalable(0.6f, 0.6f)) 
+                    .build()
+    );
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        FabricDefaultAttributeRegistry.register(HAT_ENTITY, HatEntity.createMobAttributes());
+    }
+}
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
